123b22637e86b9da32cee02258947970e0cb8a7d
Nahuel <schimpfn@gmail.com>
2019-06-06 21:26:36 -0300

    practica1

---
+++ coreEscuela/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace coreEscuela
+{
+    class Escuela
+    {
+        public String nombre;
+        public String ciudad;
+        public int añoFundacion;
+        public String ceo;
+        public void Timbrar()
+        {
+            Console.Beep(2000,3000);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var miEscuela = new Escuela();
+            miEscuela.nombre = "Ptalzi Academy";
+            miEscuela.ciudad = "Pizarro 7270";
+            miEscuela.añoFundacion = 2012;
+            Console.WriteLine("TIMBRE");
+            miEscuela.Timbrar();
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ platzihelloworld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace platzihelloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+        }
+    }
+}
